4ab3e921d3fd290d3a2b63d92101adb7ef91751f
Wayne Lai <64002360+wayne-06@users.noreply.github.com>
2024-02-08 18:12:37 +0800

    fix: register route

--- routes/auth.php
+++ routes/auth.php
@@ -12,8 +12,6 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-    Route::get('register', [RegisteredUserController::class, 'create'])
-                ->name('register');
 
     Route::get('register/{referral?}', [RegisteredUserController::class, 'create'])->name('register');
 

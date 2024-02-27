c43bc9e75da93236c2cdb06b3018e7573483e202
Wayne Lai <64002360+wayne-06@users.noreply.github.com>
2024-02-27 12:30:29 +0800

    fix: withdrawal from account min

--- app/Http/Requests/WithdrawBalanceRequest.php
+++ app/Http/Requests/WithdrawBalanceRequest.php
@@ -11,7 +11,7 @@ class WithdrawBalanceRequest extends FormRequest
         return [
             'to_wallet_id' => ['required'],
             'from_meta_login' => ['required'],
-            'amount' => ['required', 'numeric', 'min:30'],
+            'amount' => ['required', 'numeric', 'gt:0'],
         ];
     }
 

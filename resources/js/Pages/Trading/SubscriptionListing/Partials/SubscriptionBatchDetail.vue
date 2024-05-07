f0d18c0c34af597c834ba005824898c53c7d6cc0
Wayne Lai <waynelai06@gmail.com>
2024-05-07 15:38:28 +0800

    fix: terminate fee details

--- resources/js/Pages/Trading/SubscriptionListing/Partials/SubscriptionBatchDetail.vue
+++ resources/js/Pages/Trading/SubscriptionListing/Partials/SubscriptionBatchDetail.vue
@@ -56,126 +56,128 @@ const calculateWidthPercentage = (starting_date, expired_date, period) => {
 </script>
 
 <template>
-    <div class="p-5 bg-gray-100 dark:bg-gray-600 rounded-lg">
-        <div class="flex flex-col items-start gap-3 self-stretch">
-            <div class="flex items-center justify-between gap-2 self-stretch">
-                <div class="text-lg font-semibold dark:text-white">
-                    <div v-if="currentLocale === 'en'">
-                        {{ subscription.master.trading_user.name }} - {{ subscription.master_meta_login }}
+    <div class="flex flex-col gap-5">
+        <div class="p-5 bg-gray-100 dark:bg-gray-600 rounded-lg">
+            <div class="flex flex-col items-start gap-3 self-stretch">
+                <div class="flex items-center justify-between gap-2 self-stretch">
+                    <div class="text-lg font-semibold dark:text-white">
+                        <div v-if="currentLocale === 'en'">
+                            {{ subscription.master.trading_user.name }} - {{ subscription.master_meta_login }}
+                        </div>
+                        <div v-if="currentLocale === 'cn'">
+                            {{ subscription.master.trading_user.company ? subscription.master.trading_user.company : subscription.master.trading_user.name }} - {{ subscription.master_meta_login }}
+                        </div>
                     </div>
-                    <div v-if="currentLocale === 'cn'">
-                        {{ subscription.master.trading_user.company ? subscription.master.trading_user.company : subscription.master.trading_user.name }} - {{ subscription.master_meta_login }}
+                    <StatusBadge
+                        :value="subscription.status"
+                        width="w-20"
+                    />
+                </div>
+                <div class="flex items-center justify-between gap-2 self-stretch">
+                    <div class="font-semibold text-xs sm:text-sm text-gray-500 dark:text-gray-400">
+                        {{$t('public.account_number')}}
                     </div>
-                </div>
-                <StatusBadge
-                    :value="subscription.status"
-                    width="w-20"
-                />
-            </div>
-            <div class="flex items-center justify-between gap-2 self-stretch">
-                <div class="font-semibold text-xs sm:text-sm text-gray-500 dark:text-gray-400">
-                    {{$t('public.account_number')}}
-                </div>
-                <div class="text-sm sm:text-base text-gray-800 dark:text-white font-semibold">
-                    {{ subscription.meta_login }}
-                </div>
-            </div>
-            <div class="flex items-center justify-between gap-2 self-stretch">
-                <div class="font-semibold text-xs sm:text-sm text-gray-500 dark:text-gray-400">
-                    {{$t('public.subscription_number')}}
-                </div>
-                <div class="text-sm sm:text-base text-gray-800 dark:text-white font-semibold">
-                    {{ subscription.subscription_number }}
-                </div>
-            </div>
-            <div class="flex items-center justify-between gap-2 self-stretch">
-                <div class="font-semibold text-xs sm:text-sm text-gray-500 dark:text-gray-400">
-                    {{$t('public.join_date')}}
-                </div>
-                <div class="text-sm sm:text-base text-gray-800 dark:text-white font-semibold">
-                    {{ formatDateTime(subscription.approval_date, false) }}
-                </div>
-            </div>
-            <div class="flex items-center justify-between gap-2 self-stretch">
-                <div class="font-semibold text-xs sm:text-sm text-gray-500 dark:text-gray-400">
-                    {{$t('public.join_day')}}
-                </div>
-                <div class="text-sm sm:text-base text-gray-800 dark:text-white font-semibold">
-                    {{ subscription.join_days }} {{ $t('public.days') }} / {{ subscription.management_period }} {{ $t('public.days') }}
-                </div>
-            </div>
-            <div class="flex items-start justify-between gap-2 self-stretch">
-                <div class="font-semibold text-sm text-gray-500 dark:text-gray-400">
-                    {{$t('public.amount')}}
-                </div>
-                <div class="text-sm sm:text-base text-primary-500 dark:text-primary-400 font-bold">
-                    $ {{ formatAmount(subscription.meta_balance) }}
-                </div>
-            </div>
-            <div
-                v-if="batchDetail && subscription.status === 'Terminated'"
-                class="flex items-start justify-between gap-2 self-stretch"
-            >
-                <div class="font-semibold text-sm text-gray-500 dark:text-gray-400">
-                    {{$t('public.management_fee')}} (<span :class="{'text-error-500': batchDetail.penalty_percent > 0}">{{ formatAmount(batchDetail.penalty_percent, 0) }}%</span>)
-                </div>
-                <div class="text-sm sm:text-base text-error-500 font-bold">
-                    $ {{ formatAmount(batchDetail ? batchDetail.penalty_amount : 0) }}
-                </div>
-            </div>
-            <div
-                v-if="batchDetail && subscription.status === 'Terminated'"
-                class="flex items-start justify-between gap-2 self-stretch"
-            >
-                <div class="font-semibold text-sm text-gray-500 dark:text-gray-400">
-                    {{$t('public.return_amount')}}
-                </div>
-                <div class="text-sm sm:text-base text-success-500 font-bold">
-                    $ {{ formatAmount(subscription.meta_balance - (batchDetail ? batchDetail.penalty_amount : 0)) }}
-                </div>
-            </div>
-            <div
-                v-if="batchDetail && subscription.status === 'Terminated'"
-                class="flex items-start justify-between gap-2 self-stretch"
-            >
-                <div class="font-semibold text-sm text-gray-500 dark:text-gray-400">
-                    {{$t('public.termination_date')}}
-                </div>
-                <div class="text-sm sm:text-base text-error-500 font-bold">
-                    {{ formatDateTime(subscription ? subscription.termination_date : '-', false) }}
-                </div>
-            </div>
-            <div
-                v-if="subscription.status !== 'Terminated'"
-                class="flex items-center justify-between gap-2 self-stretch"
-            >
-                <div class="font-semibold text-xs sm:text-sm text-gray-500 dark:text-gray-400">
-                    {{$t('public.auto_renewal')}}
-                </div>
-                <div class="text-sm sm:text-base text-gray-800 dark:text-white font-semibold">
-                    <Badge :variant="statusVariant(subscription.auto_renewal)">{{ subscription.auto_renewal === 1 ? $t('public.auto') : $t('public.expiring') }}</Badge>
-                </div>
-            </div>
-            <div
-                v-if="subscription.status !== 'Terminated'"
-                class="flex flex-col gap-2 self-stretch"
-            >
-                <div class="font-semibold text-sm text-gray-500 dark:text-gray-400">
-                    {{$t('public.progress')}}
-                </div>
-                <div class="mb-1 flex h-2.5 w-full overflow-hidden rounded-full bg-gray-300 dark:bg-gray-400 text-xs">
-                    <div
-                        :style="{ width: `${calculateWidthPercentage(subscription.approval_date, subscription.settlement_date, subscription.subscription_period).widthResult}%` }"
-                        class="rounded-full bg-gradient-to-r from-primary-300 to-success-400 dark:from-primary-400 dark:to-success-500 transition-all duration-500 ease-out"
-                    >
+                    <div class="text-sm sm:text-base text-gray-800 dark:text-white font-semibold">
+                        {{ subscription.meta_login }}
                     </div>
                 </div>
-                <div class="mb-2 flex items-center justify-between text-xs">
-                    <div class="dark:text-gray-400">
+                <div class="flex items-center justify-between gap-2 self-stretch">
+                    <div class="font-semibold text-xs sm:text-sm text-gray-500 dark:text-gray-400">
+                        {{$t('public.subscription_number')}}
+                    </div>
+                    <div class="text-sm sm:text-base text-gray-800 dark:text-white font-semibold">
+                        {{ subscription.subscription_number }}
+                    </div>
+                </div>
+                <div class="flex items-center justify-between gap-2 self-stretch">
+                    <div class="font-semibold text-xs sm:text-sm text-gray-500 dark:text-gray-400">
+                        {{$t('public.join_date')}}
+                    </div>
+                    <div class="text-sm sm:text-base text-gray-800 dark:text-white font-semibold">
                         {{ formatDateTime(subscription.approval_date, false) }}
                     </div>
-                    <div class="dark:text-gray-400">
-                        {{ formatDateTime(subscription.settlement_date, false) }}
+                </div>
+                <div class="flex items-center justify-between gap-2 self-stretch">
+                    <div class="font-semibold text-xs sm:text-sm text-gray-500 dark:text-gray-400">
+                        {{$t('public.join_day')}}
+                    </div>
+                    <div class="text-sm sm:text-base text-gray-800 dark:text-white font-semibold">
+                        {{ subscription.join_days }} {{ $t('public.days') }} / {{ subscription.management_period }} {{ $t('public.days') }}
+                    </div>
+                </div>
+                <div class="flex items-start justify-between gap-2 self-stretch">
+                    <div class="font-semibold text-sm text-gray-500 dark:text-gray-400">
+                        {{$t('public.amount')}}
+                    </div>
+                    <div class="text-sm sm:text-base text-primary-500 dark:text-primary-400 font-bold">
+                        $ {{ formatAmount(subscription.meta_balance) }}
+                    </div>
+                </div>
+                <div
+                    v-if="batchDetail && subscription.status === 'Terminated'"
+                    class="flex items-start justify-between gap-2 self-stretch"
+                >
+                    <div class="font-semibold text-sm text-gray-500 dark:text-gray-400">
+                        {{$t('public.management_fee')}} (<span :class="{'text-error-500': batchDetail.penalty_percent > 0}">{{ formatAmount(batchDetail && Object.keys(batchDetail).length !== 0 ? batchDetail.penalty_percent : 0, 0) }}%</span>)
+                    </div>
+                    <div class="text-sm sm:text-base text-error-500 font-bold">
+                        $ {{ formatAmount(batchDetail && Object.keys(batchDetail).length !== 0 ? batchDetail.penalty_amount : 0) }}
+                    </div>
+                </div>
+                <div
+                    v-if="batchDetail && subscription.status === 'Terminated'"
+                    class="flex items-start justify-between gap-2 self-stretch"
+                >
+                    <div class="font-semibold text-sm text-gray-500 dark:text-gray-400">
+                        {{$t('public.return_amount')}}
+                    </div>
+                    <div class="text-sm sm:text-base text-success-500 font-bold">
+                        $ {{ formatAmount(subscription.meta_balance - (batchDetail && Object.keys(batchDetail).length !== 0 ? batchDetail.penalty_amount : 0)) }}
+                    </div>
+                </div>
+                <div
+                    v-if="batchDetail && subscription.status === 'Terminated'"
+                    class="flex items-start justify-between gap-2 self-stretch"
+                >
+                    <div class="font-semibold text-sm text-gray-500 dark:text-gray-400">
+                        {{$t('public.termination_date')}}
+                    </div>
+                    <div class="text-sm sm:text-base text-error-500 font-bold">
+                        {{ formatDateTime(subscription ? subscription.termination_date : '-', false) }}
+                    </div>
+                </div>
+                <div
+                    v-if="subscription.status !== 'Terminated'"
+                    class="flex items-center justify-between gap-2 self-stretch"
+                >
+                    <div class="font-semibold text-xs sm:text-sm text-gray-500 dark:text-gray-400">
+                        {{$t('public.auto_renewal')}}
+                    </div>
+                    <div class="text-sm sm:text-base text-gray-800 dark:text-white font-semibold">
+                        <Badge :variant="statusVariant(subscription.auto_renewal)">{{ subscription.auto_renewal === 1 ? $t('public.auto') : $t('public.expiring') }}</Badge>
+                    </div>
+                </div>
+                <div
+                    v-if="subscription.status !== 'Terminated'"
+                    class="flex flex-col gap-2 self-stretch"
+                >
+                    <div class="font-semibold text-sm text-gray-500 dark:text-gray-400">
+                        {{$t('public.progress')}}
+                    </div>
+                    <div class="mb-1 flex h-2.5 w-full overflow-hidden rounded-full bg-gray-300 dark:bg-gray-400 text-xs">
+                        <div
+                            :style="{ width: `${calculateWidthPercentage(subscription.approval_date, subscription.settlement_date, subscription.subscription_period).widthResult}%` }"
+                            class="rounded-full bg-gradient-to-r from-primary-300 to-success-400 dark:from-primary-400 dark:to-success-500 transition-all duration-500 ease-out"
+                        >
+                        </div>
+                    </div>
+                    <div class="mb-2 flex items-center justify-between text-xs">
+                        <div class="dark:text-gray-400">
+                            {{ formatDateTime(subscription.approval_date, false) }}
+                        </div>
+                        <div class="dark:text-gray-400">
+                            {{ formatDateTime(subscription.settlement_date, false) }}
+                        </div>
                     </div>
                 </div>
             </div>

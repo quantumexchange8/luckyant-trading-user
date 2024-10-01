28e6d026e75e5622cdf9a9811e04c085dce8b586
Wayne Lai <waynelai06@gmail.com>
2024-10-01 09:34:30 +0800

    fix: apple icon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,6 +10,9 @@
 
         <title inertia>{{ trans('public.luckyant-trading') }}</title>
 
+        <link rel="shortcut icon" type="image/x-icon" href="{{ asset('favicon.ico')}}">
+        <link rel="apple-touch-icon" href="{{ asset('favicon.ico')}}">
+
         <!-- Fonts -->
         <link
             href="https://fonts.googleapis.com/css2?family=Inter:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap"

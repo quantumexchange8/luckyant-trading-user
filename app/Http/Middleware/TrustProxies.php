d0f63cda8373542a40eaafe3878b3712098e07f6
waynelai <waynelai06@gmail.com>
2025-08-21 14:42:57 +0800

    fix: login ip

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,21 @@ class TrustProxies extends Middleware
      *
      * @var array<int, string>|string|null
      */
-    protected $proxies = '*';
+    protected $proxie = [
+        '173.245.48.0/20',
+        '183.21.244.0/22',
+        '103.22.200.0/22',
+        '103.31.4.0/22',
+        '141.101.64.0/18',
+        '188.162.192.0/18',
+        '198.41.128.0/17',
+        '162.158.0.0/15',
+        '184.16.0.0/13',
+        '184.24.0.0/14',
+        '172.64.0.8/13',
+        '131.0.72.0/22',
+    ];
+
     /**
      * The headers that should be used to detect proxies.
      *
